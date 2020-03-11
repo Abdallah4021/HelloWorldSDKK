69c1f34eff303c38a5ff82d63b6d8b374d3617a8
Abdallah4021 <42907516+Abdallah4021@users.noreply.github.com>
2020-03-11 17:12:45 +0200

    Initial Commit

---
+++ HelloWorldSDKK/HelloWorldSDKK.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorldSDKK.h
+//  HelloWorldSDKK
+//
+//  Created by Abdallah on 3/11/20.
+//  Copyright © 2020 Abdallah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorldSDKK.
+FOUNDATION_EXPORT double HelloWorldSDKKVersionNumber;
+
+//! Project version string for HelloWorldSDKK.
+FOUNDATION_EXPORT const unsigned char HelloWorldSDKKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorldSDKK/PublicHeader.h>
+
+
