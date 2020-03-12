0cb1de6e53302ca911686ad775315a2140285c8b
Abdallah4021 <42907516+Abdallah4021@users.noreply.github.com>
2020-03-12 09:56:00 +0200

    add LICENCE

---
+++ HelloWorldSDKK/HelloWorld.swift
@@ -0,0 +1,21 @@
+//
+//  HelloWorld.swift
+//  HelloWorldSDKK
+//
+//  Created by Abdallah on 3/11/20.
+//  Copyright © 2020 Abdallah. All rights reserved.
+//
+
+import Foundation
+
+
+public class HelloWorld {
+    let hello = "Hello"
+    
+    public init(){}
+    
+    public func hello(to whom:String)->String{
+        return"Hello \(whom)"
+    }
+    
+}
